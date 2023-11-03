16439797cce5b98323f130d818585530e592d453
Klw_m <97058978+Clwmm@users.noreply.github.com>
2023-11-03 12:57:35 +0100

    z3

---
+++ Zestaw03/Generator.cpp
@@ -0,0 +1,65 @@
+// Mateusz Kałwa
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <random>
+
+auto main(int argc, char* argv[]) -> int
+{
+    std::ios::sync_with_stdio(false);
+    // Input error handling
+    if (argc == 1) {
+        std::cerr << "Specify the number of oprations" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (argc > 2) {
+        std::cerr << "Incorrect arguments were given" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int number_of_operations = std::stoi(argv[1]);
+
+    // Input error handling
+    if (number_of_operations < 0 || number_of_operations > 1000000)
+    {
+        std::cerr << "Incorrect arguments were given" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Setting the range of random numbers
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> r_num(0, 10000);
+    std::uniform_int_distribution<int> r_type(0, 5);
+    
+    // Printing the first number as the number of operations
+    std::cout << number_of_operations << std::endl;
+
+    for (int i = 0; i < number_of_operations; ++i)
+    {
+        // Generating a random number that selects the type of writeout
+        switch (r_type(gen))
+        {
+        case 0:
+            std::cout << "F " << r_num(gen) << std::endl;
+            break;
+        case 1:
+            std::cout << "B " << r_num(gen) << std::endl;
+            break;
+        case 2:
+            std::cout << "f" << std::endl;
+            break;
+        case 3:
+            std::cout << "b" << std::endl;
+            break;
+        case 4:
+            std::cout << "R " << r_num(gen) << " " << r_num(gen) << std::endl;
+            break;
+        case 5:
+            std::cout << "S" << std::endl;
+            break;
+        default:
+            break;
+        }
+    }
+}
---
+++ Zestaw03/LinkedList.cpp
@@ -0,0 +1,111 @@
+// Mateusz Kałwa
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdexcept>
+#include "LinkedList.h"
+
+auto main() -> int
+{
+    LinkedList<int> list;
+
+    // Create additional variables to pick up and store words given on standard input
+    std::string line;
+    std::string word;
+
+    // Get the first value that determines the number of operations
+    std::getline(std::cin, line);
+    int n = 0;
+    std::istringstream iss_f(line);
+    iss_f >> word;
+    n = std::stoi(word);
+
+    // Input error handling
+    if (n < 0 || n > 1000000)
+    {
+        std::cerr << "Incorrect arguments were given" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    for (int k = 0; k < n; ++k)
+    {
+        // Getting lines from the std::cin and splitting them into separate words 
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+
+        std::vector<std::string> words;
+
+        while (iss >> word)
+        {
+            words.push_back(word);
+        }
+
+        // Program logic as intended
+        if (words[0] == "F")
+        {
+            list.push_front(std::stoi(words[1]));
+            continue;
+        }
+        else if (words[0] == "B")
+        {
+            list.push_back(std::stoi(words[1]));
+            continue;
+        }
+        else if (words[0] == "f")
+        {
+            if (list.empty())
+            {
+                std::cout << "EMPTY" << std::endl;
+                continue;
+            }
+            else
+            {
+                std::cout << list.pop_front() << std::endl;
+                continue;
+            }
+        }
+        else if (words[0] == "b")
+        {
+            if (list.empty())
+            {
+                std::cout << "EMPTY" << std::endl;
+                continue;
+            }
+            else
+            {
+                std::cout << list.pop_back() << std::endl;
+                continue;
+            }
+        }
+        else if (words[0] == "R")
+        {
+            auto it = list.begin();
+            try {
+                it = list.find(std::stoi(words[1]));
+            }
+            catch (std::exception& e) {
+                std::cout << "FALSE" << std::endl;
+                continue;
+            }
+
+            *it = std::stoi(words[2]);
+            std::cout << "TRUE" << std::endl;
+            continue;
+        }
+        else if (words[0] == "S")
+        {
+            std::cout << list.size() << std::endl;
+            continue;
+        }
+        else
+        {
+            std::cerr << "Error input" << std::endl;
+            --n;
+        }
+
+    }
+}

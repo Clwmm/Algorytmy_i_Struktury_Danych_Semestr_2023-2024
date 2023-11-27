ef04508f23200050f68aef6f5a975d59e70d1e39
Klw_m <97058978+Clwmm@users.noreply.github.com>
2023-11-27 12:28:45 +0100

    Complexity change

--- Zestaw05/Complexity.cpp
+++ Zestaw05/Complexity.cpp
@@ -2,97 +2,69 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
 
 // Bubble sort
 template <class T>
-int swap_sort_count(std::vector<T>& v)
+std::map<std::string, int> sort(std::vector<T>& v)
 {
 	int number_of_swaps = 0;
+	int number_of_ifs = 0;
+
 	for (int it = 0; it < v.size(); ++it)
+	{
 		for (int j = 0; j < v.size() - 1; ++j)
+		{
+			++number_of_ifs;
 			if (v[j] > v[j + 1])
 			{
 				std::swap(v[j], v[j + 1]);
 				++number_of_swaps;
 			}
-	return number_of_swaps;
-}
-
-template <class T>
-int if_sort_count(std::vector<T>& v)
-{
-	int number_of_ifs = 0;
-	for (int it = 0; it < v.size(); ++it)
-		for (int j = 0; j < v.size() - 1; ++j)
-		{
-			++number_of_ifs;
-			if (v[j] > v[j + 1])
-				std::swap(v[j], v[j + 1]);
 		}
-			
-	return number_of_ifs;
+	}	
+	return { {"swap", number_of_swaps}, {"if", number_of_ifs} };
 }
 
 auto main() -> int
 {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> r_num(1, 1000000);
+	
 	std::vector<int> vec;
+	int n = 1000;
 
-	std::cout << "Bubble Sort:" << std::endl << std::endl;
-	
-	std::cout << "Checking number of ifs:" << std::endl << std::endl;
-
-	// Very optimistic situation
-	for (int i = 0; i < 100; ++i)
-		vec.push_back(i);
-	std::swap(vec[1], vec[0]);
-
-	std::cout << "Optimistic situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << if_sort_count<int>(vec) << std::endl << std::endl;
-	vec.clear();
+	std::cout << "Bubble Sort: [ n = " << n << " ]" << std::endl << std::endl << std::endl;
 
 	// Optimistic situation
-	for (int i = 1; i < 100; ++i)
+	for (int i = 0; i < n; ++i)
 		vec.push_back(i);
-	vec.push_back(0);
 
-	std::cout << "Average situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << if_sort_count<int>(vec) << std::endl << std::endl;
+	std::map<std::string, int> dominants = sort<int>(vec);
+
+	std::cout << "Optimistic situation:" << std::endl << std::endl;
+	std::cout << "\tDominant operations:\n\t\tIf:\t" << dominants["if"] << "\n\t\tSwap:\t" << dominants["swap"] << std::endl << std::endl;
+	vec.clear();
+
+	// Avrage situation
+	for (int i = 0; i < n; ++i)
+		vec.push_back(r_num(gen));
+
+	dominants = sort<int>(vec);
+
+	std::cout << "Avrage situation:" << std::endl << std::endl;
+	std::cout << "\tDominant operations:\n\t\tIf:\t" << dominants["if"] << "\n\t\tSwap:\t" << dominants["swap"] << std::endl << std::endl;
 	vec.clear();
 
 	// Pessimistic situation
-	for (int i = 99; i >= 0; --i)
+	for (int i = n - 1; i >= 0; --i)
 		vec.push_back(i);
 
-	std::cout << "Pessimistic situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << if_sort_count<int>(vec) << std::endl << std::endl;
-	vec.clear();
+	dominants = sort<int>(vec);
 
-	std::cout << "Checking number of swaps:" << std::endl << std::endl;
-
-	// Very optimistic situation
-	for (int i = 0; i < 100; ++i)
-		vec.push_back(i);
-	std::swap(vec[1], vec[0]);
-
-	std::cout << "Optimistic situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << swap_sort_count<int>(vec) << std::endl << std::endl;
-	vec.clear();
-
-	// Optimistic situation
-	for (int i = 1; i < 100; ++i)
-		vec.push_back(i);
-	vec.push_back(0);
-	
-	std::cout << "Average situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << swap_sort_count<int>(vec) << std::endl << std::endl;
-	vec.clear();
-
-	// Pessimistic situation
-	for (int i = 99; i >= 0; --i)
-		vec.push_back(i);
-
-	std::cout << "Pessimistic situation:" << std::endl;
-	std::cout << "n: " << vec.size() << "\tDominant operations: " << swap_sort_count<int>(vec) << std::endl << std::endl;
+	std::cout << "Pessimistic situation:" << std::endl << std::endl;
+	std::cout << "\tDominant operations:\n\t\tIf:\t" << dominants["if"] << "\n\t\tSwap:\t" << dominants["swap"] << std::endl << std::endl;
 	vec.clear();
 }
